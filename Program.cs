1234ac6a770545a0ae167b98c07deb174c2a9550
diliprathod32 <diliprathod32@gmail.com>
2021-02-02 17:00:30 +0530

    [Dilip] EmployeeAttendanceP/A

--- Program.cs
+++ Program.cs
@@ -7,6 +7,15 @@ namespace EmpWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program on Master Branch!");
+            int isPresent = 1;
+            Random randomNumber = new Random();
+            int empCheck = randomNumber.Next(0,2);
+            Console.WriteLine(empCheck);
+            if (empCheck == isPresent)
+                Console.WriteLine("present");
+
+              else
+                Console.WriteLine("absent");
         }
     }
 }
